f3e13c780fb17a05d36049e70c200ba0e164d5da
ian <ian.murphy92@gmail.com>
2022-05-18 16:33:37 -0400

    commit tree 01d676c267d1a2418826b02956240f784e1ff38f

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(cb){
+    return cb();
+
+}
+
+
+function returnsANamedFunction(fn){
+    fn = returnsANamedFunction
+    return fn
+}
+function returnsAnAnonymousFunction () {
+    return function(){}
+}
